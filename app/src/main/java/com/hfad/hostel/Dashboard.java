518d90fada69d07ff6eb3d449d95f92e6f2f3ad9
MEET <mmangukiya927@rku.ac.in>
2022-09-06 23:14:10 +0530

    login and log out

--- app/src/main/java/com/hfad/hostel/Dashboard.java
+++ app/src/main/java/com/hfad/hostel/Dashboard.java
@@ -4,24 +4,30 @@ package com.hfad.hostel;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
-import androidx.core.splashscreen.SplashScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.data.DataHolder;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
     ImageView Profile;
     CardView gatepass;
     Toolbar toolbar;
-    TextView txttoolbar;
+    TextView txttoolbar,signout;
     ImageView ic_back;
+    FirebaseAuth mAuth;
+    Button out;
 
     BottomSheetDialog sheetDialog;
     @Override
@@ -32,6 +38,9 @@ public class Dashboard extends AppCompatActivity {
 
         Profile=findViewById(R.id.Profile);
         gatepass=findViewById(R.id.gatepass);
+        signout=findViewById(R.id.tv_signout);
+        mAuth=FirebaseAuth.getInstance();
+        out=findViewById(R.id.btn_signout);
 
         txttoolbar = findViewById(R.id.txt_toolbar);
         txttoolbar.setText(R.string.my_dashboard);
@@ -44,6 +53,8 @@ public class Dashboard extends AppCompatActivity {
             View view1 = LayoutInflater.from(Dashboard.this).inflate(R.layout.bottomsheet_dialog,(LinearLayout)findViewById(R.id.sheet));
             sheetDialog.setContentView(view1);
             sheetDialog.show();
+
+
         });
 
        gatepass.setOnClickListener(new View.OnClickListener() {
@@ -53,5 +64,26 @@ public class Dashboard extends AppCompatActivity {
                startActivity(i);
            }
        });
+
+       out.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               mAuth.signOut();
+               Intent i =new Intent(Dashboard.this,login.class);
+               startActivity(i);
+           }
+       });
+
+    }
+//If user click back button then they can't redirect to login page
+    int counter=0;
+
+    public void onBackPressed(){
+        counter++;
+        if(counter ==2){
+            super.onBackPressed();
+        }else{
+            Toast.makeText(this,"Press back again to exit",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/hfad/hostel/login.java
+++ app/src/main/java/com/hfad/hostel/login.java
@@ -1,14 +1,123 @@
 package com.hfad.hostel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+    public Button login;
+    public TextInputLayout username, password;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    android.app.ProgressDialog ProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btn_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+        ProgressDialog=new ProgressDialog(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                performLogin();
+                validateusername();
+                validatepassword();
+            }
+        });
+    }
+
+    private Boolean validateusername(){
+        String val=username.getEditText().getText().toString();
+        if(val==null){
+            username.setError("Field can't be empty");
+            return false;
+        }
+        else{
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatepassword(){
+        String val=password.getEditText().getText().toString();
+        if(val==null){
+            username.setError("Field can't be empty");
+            return false;
+        }
+        else{
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private void performLogin() {
+        String user=username.getEditText().getText().toString();
+        String pass=password.getEditText().getText().toString();
+        ProgressDialog.setMessage("Please wait while login...");
+        ProgressDialog.setTitle("Login");
+        ProgressDialog.setCanceledOnTouchOutside(false);
+        ProgressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i=new Intent(login.this,Dashboard.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    Toast.makeText(login.this,"Login Successfully.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(login.this,""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if(mUser != null) {
+            // User is signed in
+            Intent i = new Intent(login.this, Dashboard.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        }
+        else{
+
+            Intent i = new Intent(login.this, login.class);
+        }
+
     }
 }
+
+
+    //Sign out user
+//FirebaseAuth.getInstance().signOut();
