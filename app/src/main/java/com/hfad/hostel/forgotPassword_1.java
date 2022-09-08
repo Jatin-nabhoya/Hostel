1ce96727417c90f45dd940e0dec60cdb2a5cbbba
JATIN NABHOYA <jnabhoya474@rku.ac.in>
2022-09-08 18:09:14 +0530

    new design

--- app/src/main/java/com/hfad/hostel/forgotPassword_1.java
+++ app/src/main/java/com/hfad/hostel/forgotPassword_1.java
@@ -3,12 +3,23 @@ package com.hfad.hostel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class forgotPassword_1 extends AppCompatActivity {
+    ImageView ic_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password1);
+
+        ic_back =findViewById(R.id.ic_back);
+        ic_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hfad/hostel/forgot_password2.java
@@ -0,0 +1,15 @@
+package com.hfad.hostel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_password2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password2);
+
+    }
+}
